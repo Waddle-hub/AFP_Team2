594972d1d9d0183e826aae5cb528c27cb0bc6caa
said <eger3612@gmail.com>
2020-10-12 15:13:26 -0700

    edit

--- Project_2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Project_2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -72,6 +72,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(182, 31);
             this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
