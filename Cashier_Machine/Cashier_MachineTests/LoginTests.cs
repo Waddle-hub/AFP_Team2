b83c1641c1379d8a26551cfef109eaa26cb6628b
Mxolisi <silabelamxolisi@gmail.com>
2020-10-14 23:39:57 +0200

    Login Test case

---
+++ Cashier_Machine/Cashier_MachineTests/LoginTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier_MachineTests
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        [Test]
+        public void UserWithIncorrectUserAndPassword()
+        {
+            //Arrange
+            string username = "";
+            string password = "";
+
+            //Act
+            string result = "Incorrect Pass or Username used";
+
+            //Assert
+            Assert.Pass(result);
+        }
+    }
+}
